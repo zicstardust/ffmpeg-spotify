a2e39295ebee99df562fa5c2946da6bec69d9f57
Zic <git@zicstardust.com>
2026-01-11 10:18:51 -0400

    add install

---
+++ README.md
@@ -0,0 +1,14 @@
+# FFMPEG Libraries to Spotify Linux Client
+
+FFMPEG libraries for the native Linux Spotify client. To resolve the issue of playing local files on distributions (exemple: debian 13) that do not have the correct version of the FFMPEG libraries.
+
+## Install
+
+### Dependecies
+- curl
+- bash
+
+### Install
+```sh
+curl https://raw.githubusercontent.com/zicstardust/ffmpeg-spotify/main/install.sh > /tmp/spotifyFFMPEG.sh; bash /tmp/spotifyFFMPEG.sh
+```
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+current_dir=$(pwd)
+
+mkdir -p ${HOME}/.local/share/applications
+mkdir -p ${HOME}/.local/share/spotifyffmpeg
+
+
+cd ${HOME}/.local/share/spotifyffmpeg
+curl -fSL "https://github.com/zicstardust/ffmpeg-spotify/releases/download/1.0/spotify_ffmpeg_libs_linux_x86_64.tar.gz" -o spotify_ffmpeg_libs_linux_x86_64.tar.gz &> /dev/null
+tar -xf spotify_ffmpeg_libs_linux_x86_64.tar.gz
+rm -f spotify_ffmpeg_libs_linux_x86_64.tar.gz
+chmod -R +x *
+
+cat > ${HOME}/.local/share/applications/spotify-ffmpeg.desktop <<DESKTOP
+[Desktop Entry]
+Type=Application
+Name=Spotify (fix FFMPEG)
+GenericName=Music Player
+Comment=Spotify streaming music client fix FFMPEG
+Icon=spotify-client
+Exec=LD_LIBRARY_PATH="${HOME}/.local/share/spotifyffmpeg\${LD_PRELOAD:+:\$LD_PRELOAD}" spotify %U
+Terminal=false
+MimeType=x-scheme-handler/spotify;
+Categories=Audio;Music;Player;AudioVideo;
+StartupWMClass=spotify
+DESKTOP
+
+cd $current_dir
